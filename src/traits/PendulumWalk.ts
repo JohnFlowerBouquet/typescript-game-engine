2dcf34eda56b6c6bda9ec657196aea58a4ae8ac8
John <john.flower.bouquet@gmail.com>
2021-12-09 21:18:13 +0100

    feat: Added PendulumWalk trait

---
+++ src/traits/PendulumWalk.ts
@@ -0,0 +1,18 @@
+import Entity, { Side } from "../Entity";
+import Trait from "./Trait";
+
+export default class PendulumWalk extends Trait {
+    constructor(private _speed: number = 30) {
+        super('pendulumWalk');
+    }
+
+    public update(entity: Entity, deltaTime: number): void {
+        entity.velocity.x = this._speed;
+    }
+
+    public obstruct(entity: Entity, side: Side): void {
+        if (side === Side.left || side === Side.right) {
+            this._speed = -this._speed;
+        }
+    }
+}
